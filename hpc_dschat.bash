1859bf7013760e207c9436c698bfff2d23f8a933
Lei Chen <chen.lei.05@gmail.com>
2023-04-19 14:38:03 -0400

    Remove MoQ legacy code so that we can cfs to PVC Updated run_1.3b.sh to output to both STDOUT and log file (using tee)

---
+++ hpc_dschat.bash
@@ -0,0 +1,18 @@
+
+export http_proxy=http://pkg.proxy.prod.jp.local:10080
+export https_proxy=http://pkg.proxy.prod.jp.local:10080
+export HTTP_PROXY=http://pkg.proxy.prod.jp.local:10080
+export HTTPS_PROXY=http://pkg.proxy.prod.jp.local:10080
+
+cd /workdir/DeepSpeedExamples/applications/DeepSpeed-Chat/
+pip install deepspeed>0.9.0
+pip install -r requirements.txt
+
+# Move into the first step of the pipeline
+cd training/step1_supervised_finetuning/
+
+# Run the training script
+ulimit -n 99999 && bash training_scripts/single_node/run_1.3b.sh
+
+# Evaluate the model
+ulimit -n 99999 && bash evaluation_scripts/run_prompt.sh
